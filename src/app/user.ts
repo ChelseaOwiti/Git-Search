fe79463e2f446d5e770c1f0538f2be263f6092d8
Chelsea Owiti <chelseaowiti@gmail.com>
2019-06-09 17:14:34 -0400

    user class defined

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,19 @@
 export class User {
+  constructor(
+    public avatar_url:string, 
+    public followers_url:string,
+    public following_url:string,
+    public gists_url:string,
+    public login:string,
+    public name:string,
+    public company:string,
+    public blog:string,
+    public location:string,
+    public bio:string,
+    public public_repos:number,
+    public public_gists:string,
+    public followers:number,
+    public following:number,
+    public created_at:any,
+  ){}
 }
