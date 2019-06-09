9751dc961edfd212059ec55576663cc83bfc5eb2
Chelsea Owiti <chelseaowiti@gmail.com>
2019-06-09 18:04:48 -0400

    content added in git service, service

--- src/app/gitservice.service.ts
+++ src/app/gitservice.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../environments/environment';
+import {Repo} from './repo';
+import {User} from './user';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class GitserviceService {
 
-  constructor() { }
+  constructor( private http: HttpClient) { }
+  //getting github data
+  getUser(userName){
+    return this.http.get<User>(environment.apiUrl + userName + "?access_token=" + environment.key);
+  }
+  getRepo(userName){
+    return this.http.get<Repo>(environment.apiUrl + userName + '/repo' + "?access_token+" + environment.key);
+  }
 }
