5ee07471fd49ff25fa314d59120ab94459ee8680
Chelsea Owiti <chelseaowiti@gmail.com>
2019-06-10 11:15:07 -0400

    parameters passed in Routes array

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
+import { GitserviceService } from '../gitservice.service';
+import { Repo } from '../repo';
+import { User } from '../user';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"User", component:User},
+  {path: "Repo", component:Repo}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
