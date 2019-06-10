60858bea6326c62d8b1013c1b112abd696f89b0f
Chelsea Owiti <chelseaowiti@gmail.com>
2019-06-09 20:51:13 -0400

    backend info on my data added

--- src/app/landpage/landpage.component.ts
+++ src/app/landpage/landpage.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { GitserviceService } from '../gitservice.service';
+import { HttpClient } from '@angular/common/http';
+import { Repo } from '../repo';
 
 @Component({
   selector: 'app-landpage',
@@ -7,7 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandpageComponent implements OnInit {
 
-  constructor() { }
+  data:any;
+  repo:any;
+  username:string;
+
+  constructor(
+    private gitserviceservice: GitserviceService,
+    private http: HttpClient
+  ) { 
+    this.gitserviceservice.getUser('ChelseaOwiti').subscribe(data =>{
+      this.data= data;
+    });
+    this.gitserviceservice.getRepo('ChelseaOwiti').subscribe(repo =>{
+      this.repo = repo;
+    });
+  }
+
 
   ngOnInit() {
   }
